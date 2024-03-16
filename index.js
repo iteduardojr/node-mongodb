840064c24af55f4de3dd48291bd1e6ea604150cc
Eduardo José <it.eduardojr@gmail.com>
2024-03-15 21:21:13 -0300

    Teste

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.get('/', function(req, res){
+    res.json({})
+})
+
+app.listen(3000, function(){
+    console.log('Server UP port 3000')
+})
